3e001f915d8da667470c36692f9e8a447ed01a39
Shnaider Danila <shnd3r@yandex.ru>
2023-07-24 16:31:59 +0700

    feat: Добавить header__link в header и CSS стили

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import logo from '../images/logo.svg';
+import { Link } from 'react-router-dom';
 
-function Header() {
+function Header({linkName, routeName}) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип проекта - Место Россия" />
+      <Link to={`/${routeName}`} className="header__link" >{linkName}</Link>
     </header>
   )
 }
