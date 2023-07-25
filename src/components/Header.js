87fe228ade0f478056d2cfa631480cc747933d05
Shnaider Danila <shnd3r@yandex.ru>
2023-07-25 13:24:19 +0700

    feat: Добавить хук useLocation в компонент Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import logo from '../images/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-function Header({linkName, routeName}) {
+function Header() {
+  const location = useLocation();
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип проекта - Место Россия" />
-      <Link to={`/${routeName}`} className="header__link" >{linkName}</Link>
+      {location.pathname === "/sign-in" && <Link className="header__link" to="/sign-up">Регистрация</Link> }
+      {location.pathname === "/sign-up" && <Link className="header__link" to="/sign-in">Вход</Link> }
+      {location.pathname === "/" && <Link className="header__link" to="/sign-in">Выход</Link> }
     </header>
   )
 }
