cba42b7db5eacd977508484f71633fedabe2f439
Shnaider Danila <shnd3r@yandex.ru>
2023-07-28 13:10:22 +0700

    feat: add registration to the register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,40 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { authApi } from '../utils/Api';
 
 function Register() {
+
+  const [formValue, setFormValue] = useState({
+    email: '',
+    password: ''
+  });
+
+  const navigate = useNavigate();
+
+  function handleChange(evt) {
+    setFormValue({ ...formValue, [evt.target.name]: evt.target.value })
+  }
+
+  function handleSumbit(evt) {
+    evt.preventDefault();
+    const { email, password } = formValue;
+    authApi.register(email, password)
+      .then(() => {
+        navigate('/sign-in', {replace: true});
+      })
+      .catch((err) => {
+        console.error(`Ошибка: ${err}`);
+      });
+  }
+
   return (
     <section className="authentication">
       <h3 className="authentication__title">Регистрация</h3>
-      <form className="authentication__form">
+      <form className="authentication__form" onSubmit={handleSumbit}>
         <label className="authentication__form-field">
           <input
+            onChange={handleChange}
+            value={formValue.email}
             className="authentication__input"
             id="email"
             type="email"
@@ -16,6 +44,8 @@ function Register() {
         </label>
         <label className="authentication__form-field">
           <input
+            onChange={handleChange}
+            value={formValue.password}
             className="authentication__input"
             id="password"
             type="password"
