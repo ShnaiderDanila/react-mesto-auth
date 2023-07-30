9d5f1753048c0a111b4b37022955072872f958d2
Shnaider Danila <shnd3r@yandex.ru>
2023-07-30 15:50:47 +0700

    docs: change comments in code using JSDoc

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -4,17 +4,28 @@ class Api {
     this._headers = options.headers;
   }
 
-  // Универсальный метод обработки ответа от сервера
+  /**
+   * Универсальный метод обработки ответа от сервера
+   * @private
+   * @param {object} res объект response с сервера
+   * @returns {(json|string)} json с данными сервера или строку с ошибкой с сервера
+   */
   _renderServerResponce(res) {
     if (res.ok) {
       return res.json();
     } else {
       // если ошибка, отклоняем промис
-      return Promise.reject(`Ошибка: ${res.status}`);
+      return Promise.reject(`Ошибка: ${res.status}`); 
     }
   }
 
-  // Универсальный метод запроса с проверкой ответа
+  /**
+  * Универсальный метод запроса с проверкой ответа
+  * @private
+  * @param {string} url эндпоинт следующий за базовым URL
+  * @param {object} options объект настроек HTTP запроса
+  * @returns {(json|string)} json с данными сервера или строка с ошибкой с сервера
+  */
   _request(url, options) {
     return fetch(`${this._baseUrl}/${url}`, options)
       .then(res => {
@@ -22,26 +33,40 @@ class Api {
       })
   }
 
-  // Метод загрузки информации о пользователе с сервера
+  /**
+   * Метод загрузки информации о пользователе с сервера
+   * @return {function} возвращает результат работы this._request с переданными аргументами   
+   */
   getUserInfo() {
     return this._request(`users/me`, {
       headers: this._headers
     })
   }
 
-  // Метод загрузки карточек с сервера
+  /**
+   * Метод загрузки карточек с сервера
+   * @return {function} возвращает результат работы this._request с переданными аргументами   
+   */
   getInitialCards() {
     return this._request(`cards`, {
       headers: this._headers
     })
   }
 
-  // Метод получения общей информации (метода 1 и метода 2)
+  /**
+   * Метод получения общей информации (метода 1 и метода 2)
+   * @return {Promise} возвращает подтвержденный промис двух методов this.getInitialCards и this.getUserInfo
+   */
   getAppInfo() {
     return Promise.all([this.getInitialCards(), this.getUserInfo()]);
   }
 
-  // Метод редактирования профиля на сервере
+  /**
+   * Метод редактирования профиля на сервере
+   * @param {string} name имя пользователя страницы
+   * @param {string} about доп. информация о пользователе страницы
+   * @return {function} возвращает результат работы this._request с переданными аргументами   
+   */
   editProfile(name, about) {
     return this._request(`users/me`, {
       method: 'PATCH',
@@ -53,7 +78,12 @@ class Api {
     })
   }
 
-  // Метод добавления новой карточки на сервер
+  /**
+   * Метод добавления новой карточки на сервер
+   * @param {string} name заголовок карточки
+   * @param {string} link ссылка на картинку карточки
+   * @return {function} возвращает результат работы this._request с переданными аргументами   
+   */
   addCard(name, link) {
     return this._request(`cards`, {
       method: 'POST',
@@ -65,7 +95,11 @@ class Api {
     })
   }
 
-  // Метод удаления карточки с сервера
+  /**
+  * Метод удаления карточки с сервера
+  * @param {string} cardId свойство _id внутри объекта card
+  * @return {function} возвращает результат работы this._request с переданными аргументами   
+  */
   deleteCard(cardId) {
     return this._request(`cards/${cardId}`, {
       method: 'DELETE',
@@ -73,7 +107,12 @@ class Api {
     })
   }
 
-  // Метод изменения состояния лайка на сервере
+  /**
+  * Метод изменения состояния лайка на сервере
+  * @param {string} cardId свойство _id внутри объекта card
+  * @param {boolean} isLiked состояние лайка карточки true или false
+  * @returns {function} возвращает результат работы метода removeLike и setLike, в зависмости от состояния лайка карточки  
+  */
   changeLikeCardStatus(cardId, isLiked) {
     if (isLiked) {
       return this.removeLike(cardId)
@@ -82,7 +121,11 @@ class Api {
     }
   }
 
-  // Метод постановки лайка на сервере
+  /**
+   * Метод постановки лайка на сервере
+   * @param {string} cardId свойство _id внутри объекта card
+   * @returns {function} возвращает результат работы this._request с переданными аргументами 
+   */
   setLike(cardId) {
     return this._request(`cards/${cardId}/likes`, {
       method: 'PUT',
@@ -90,7 +133,12 @@ class Api {
     })
   }
 
-  // Метод снятия лайка на сервере
+
+  /**
+   * Метод снятия лайка на сервере
+   * @param {string} cardId свойство _id внутри объекта card
+   * @returns {function} возвращает результат работы this._request с переданными аргументами 
+   */
   removeLike(cardId) {
     return this._request(`cards/${cardId}/likes`, {
       method: 'DELETE',
@@ -98,7 +146,11 @@ class Api {
     })
   }
 
-  // Метод обновление аватара пользователя на сервере
+  /**
+  * Метод обновление аватара пользователя на сервере
+  * @param {string} avatar ссылка на картинку-аватар пользователя
+  * @returns {function} возвращает результат работы this._request с переданными аргументами 
+  */
   updateAvatar(avatar) {
     return this._request(`users/me/avatar`, {
       method: 'PATCH',
@@ -109,6 +161,12 @@ class Api {
     })
   }
 
+  /**
+   * Метод регистрации пользователя
+   * @param {string} email email пользователя из инпута формы
+   * @param {string} password пароль пользователя из инпута формы
+   * @returns {function} возвращает результат работы this._request с переданными аргументами 
+   */
   register(email, password) {
     return this._request(`signup`, {
       method: 'POST',
@@ -120,6 +178,12 @@ class Api {
     })
   }
 
+  /**
+   * Метод авторизации пользователя
+   * @param {string} email email пользователя из инпута формы
+   * @param {string} password пароль пользователя из инпута формы
+   * @returns {function} возвращает результат работы this._request с переданными аргументами 
+   */
   authorize(email, password) {
     return this._request(`signin`, {
       method: 'POST',
@@ -137,13 +201,18 @@ class Api {
       })
   }
 
+/**
+ * Метод сравнения токена клиента с токеном на сервере
+ * @param {string} token token в браузере пользователя
+ * @returns {function} возвращает результат работы this._request с переданными аргументами 
+ */
   checkToken(token) {
     return this._request(`users/me`, {
       method: 'GET',
       headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
-      } 
+      }
     })
   }
 }
